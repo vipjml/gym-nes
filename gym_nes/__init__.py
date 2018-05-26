a82af9cfc88066f3e0ec3c515d13074f34fd004a
vipjml <jml04@qq.com>
2018-05-26 18:00:56 +0800

    working...

--- gym_nes/__init__.py
+++ gym_nes/__init__.py
@@ -3,14 +3,17 @@ import math
 import gym
 from .smb_env import SuperMarioBrosEnv
 
-gym.envs.registration.register(
-    id='NesSuperMarioBros-v0',
-    entry_point='gym_nes:SuperMarioBrosEnv',
-    kwargs={
-        'max_episode_steps': math.inf,
-        'frame_skip': 4
-    },
-    nondeterministic=True,
-)
 
-__all__ = []
+def register():
+    gym.envs.registration.register(
+        id='NesSuperMarioBros-v0',
+        entry_point='gym_nes:SuperMarioBrosEnv',
+        kwargs={
+            'max_episode_steps': math.inf,
+            'frame_skip': 4
+        },
+        nondeterministic=True,
+    )
+
+
+__all__ = [register]
